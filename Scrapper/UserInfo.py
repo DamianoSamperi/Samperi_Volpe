180bd63274f279d41c8f69a6ae57414f3a72d844
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2023-12-13 11:37:56 +0100

    inserimento creazione db in UserInfo

--- Scrapper/UserInfo.py
+++ Scrapper/UserInfo.py
@@ -1,2 +1,27 @@
-def inserisci_client():
-    return
+import sqlite3
+
+conn = sqlite3.connect('users.db')
+
+# Creazione di un cursore per eseguire le query SQL
+cursor = conn.cursor()
+
+# Creazione di una tabella di esempio
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        cognome TEXT NOT NULL,
+        email TEXT NOT NULL
+    )
+''')
+
+
+def inserisci_client(nome,cognome,email):
+    cursor.execute('''
+    INSERT INTO utenti (nome, cognome, email)
+    VALUES (?, ?, ?)
+    ''', (nome, cognome, email))
+    
+    # Commit delle modifiche e chiusura della connessione
+    conn.commit()
+    conn.close()
