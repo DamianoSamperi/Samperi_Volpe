46a01513bd25402b23bd6c0194de6341a5d44b14
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2023-12-13 11:08:33 +0100

    definizione funzioni User Controller

--- Scrapper/UserController.py
+++ Scrapper/UserController.py
@@ -1,2 +1,20 @@
 #prova ulteriore, questo è lo User Controller
 #ciao Damiano
+
+def registra_client():
+    #inserisce i clienti nel database
+
+def inserisci_rules():
+    #inserisce le rules
+
+def invia_tratta():
+    #invia tratta al topic kafka
+
+def invia_aeroporto():
+    #invia aeroporto al topic kafka
+
+def autentica_client():
+    #autentica il cliente
+
+def trova_email():
+    #chiamata da elaboratore per trovare le email dei client interessati
