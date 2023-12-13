d080bb16e50754d15d5356deca39f9fc39c619e4
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2023-12-13 11:12:01 +0100

    aggiunte definizione funzioni User Info e Rules

--- Scrapper/UserController.py
+++ Scrapper/UserController.py
@@ -2,19 +2,25 @@
 #ciao Damiano
 
 def registra_client():
+    return
     #inserisce i clienti nel database
 
 def inserisci_rules():
+    return
     #inserisce le rules
 
 def invia_tratta():
+    return
     #invia tratta al topic kafka
 
 def invia_aeroporto():
+    return
     #invia aeroporto al topic kafka
 
 def autentica_client():
+    return
     #autentica il cliente
 
 def trova_email():
+    return
     #chiamata da elaboratore per trovare le email dei client interessati
