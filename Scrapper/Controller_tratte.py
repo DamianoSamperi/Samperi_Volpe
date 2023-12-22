44feff2c9c4c21006be08e437e932bc4da057e6a
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2023-12-22 15:30:32 +0100

    aggiunte eccezioni db in controller_tratte

--- Scrapper/Controller_tratte.py
+++ Scrapper/Controller_tratte.py
@@ -6,38 +6,50 @@ import requests
 
 app = Flask(__name__)
 
+try:
+    conn1 = sqlite3.connect('tratte_salvate.db')
+    conn2 = sqlite3.connect('aeroporti_salvati.db')
 
-conn1 = sqlite3.connect('tratte_salvate.db')
-conn2 = sqlite3.connect('aeroporti_salvati.db')
+    # Creazione di un cursore per eseguire le query SQL
+    cursor1 = conn1.cursor()
+    cursor2 = conn2.cursor()
+except sqlite3.Error as e:
+    print("Errore durante la connessione al database: {e}")
 
-# Creazione di un cursore per eseguire le query SQL
-cursor1 = conn1.cursor()
-cursor2 = conn2.cursor()
-cursor1.execute('''
-    CREATE TABLE IF NOT EXISTS tratte (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        origine TEXT NOT NULL,
-        destinazione TEXT NOT NULL,
-        budget INTEGER
-    )
-''')
+try:
+    cursor1.execute('''
+        CREATE TABLE IF NOT EXISTS tratte (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            origine TEXT NOT NULL,
+            destinazione TEXT NOT NULL,
+            budget INTEGER
+        )
+    ''')
+except sqlite3.Error as e:
+    print("Errore durante l'esecuzione della query: {e}")
 
-cursor2.execute('''
-    CREATE TABLE IF NOT EXISTS aeroporti (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        origine TEXT NOT NULL,
-        budget INTEGER
-    )
-''')
+try:
+    cursor2.execute('''
+        CREATE TABLE IF NOT EXISTS aeroporti (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            origine TEXT NOT NULL,
+            budget INTEGER
+        )
+    ''')
+except sqlite3.Error as e:
+    print("Errore durante l'esecuzione della query: {e}")
+    
 def leggi_database():
+    try:
+        # Esegui una query SQL
+        cursor1.execute("SELECT * FROM tratte_salvate")
+        cursor2.execute("SELECT * FROM aeroporti_salvati")
 
-    # Esegui una query SQL
-    cursor1.execute("SELECT * FROM tratte_salvate")
-    cursor2.execute("SELECT * FROM aeroporti_salvati")
-
-    # Ottieni i risultati
-    risultati = cursor1.fetchall()
-    risultati2 = cursor2.fetchall()
+        # Ottieni i risultati
+        risultati = cursor1.fetchall()
+        risultati2 = cursor2.fetchall()
+    except sqlite3.Error as e:
+        print("Errore durante l'esecuzione della query: {e}")
 
 
     # Chiudi la connessione
@@ -57,12 +69,14 @@ def leggi_database():
     return tratte,aeroporti
 
 def leggi_database_tratte():
+    try:
+        # Esegui una query SQL
+        cursor1.execute("SELECT * FROM tratte_salvate")
 
-    # Esegui una query SQL
-    cursor1.execute("SELECT * FROM tratte_salvate")
-
-    # Ottieni i risultati
-    risultati = cursor1.fetchall()
+        # Ottieni i risultati
+        risultati = cursor1.fetchall()
+    except sqlite3.Error as e:
+        print("Errore durante l'esecuzione della query: {e}")
 
 
     # Chiudi la connessione
@@ -80,12 +94,14 @@ def leggi_database_tratte():
     return tratte
 
 def leggi_database_aeroporti():
+    try:
+        # Esegui una query SQL
+        cursor2.execute("SELECT * FROM aeroporti_salvati")
 
-    # Esegui una query SQL
-    cursor2.execute("SELECT * FROM aeroporti_salvati")
-
-    # Ottieni i risultati
-    risultati = cursor2.fetchall()
+        # Ottieni i risultati
+        risultati = cursor2.fetchall()
+    except sqlite3.Error as e:
+        print("Errore durante l'esecuzione della query: {e}")
 
 
     # Chiudi la connessione
@@ -102,32 +118,36 @@ def leggi_database_aeroporti():
     return aeroporti
 
 def scrivi_database_tratte(data):
+    try:
+        # Prepara la query SQL
+        query = "REPLACE INTO tratte_salvate ( origine, destinazione ) VALUES (?, ? )" #TO_DO da modificare se vogliamo aggiungere adults
+        #TO-DO replace dovrebbe inserirlo se manca e sostituirlo se uguale
+    
 
-    # Prepara la query SQL
-    query = "REPLACE INTO tratte_salvate ( origine, destinazione ) VALUES (?, ? )" #TO_DO da modificare se vogliamo aggiungere adults
-    #TO-DO replace dovrebbe inserirlo se manca e sostituirlo se uguale
-  
+        # Esegui la query SQL con i valori passati come parametri
+        cursor1.execute(query, (data[0], data[1]))
 
-    # Esegui la query SQL con i valori passati come parametri
-    cursor1.execute(query, (data[0], data[1]))
-
-    # Esegui il commit delle modifiche
-    conn1.commit()
+        # Esegui il commit delle modifiche
+        conn1.commit()
+    except sqlite3.Error as e:
+        print("Errore durante l'esecuzione della query: {e}")
 
     # Chiudi la connessione
     #conn.close()
 
 def scrivi_database_aeroporti(data):
+    try:
+        # Prepara la query SQL
+        query = "REPLACE INTO tratte_salvate ( origine) VALUES (? )" #TO_DO da modificare se vogliamo aggiungere adults
+        #TO-DO replace dovrebbe inserirlo se manca e sostituirlo se uguale
+    
+        # Esegui la query SQL con i valori passati come parametri
+        cursor2.execute(query, (data[0]))
 
-    # Prepara la query SQL
-    query = "REPLACE INTO tratte_salvate ( origine) VALUES (? )" #TO_DO da modificare se vogliamo aggiungere adults
-    #TO-DO replace dovrebbe inserirlo se manca e sostituirlo se uguale
-  
-    # Esegui la query SQL con i valori passati come parametri
-    cursor2.execute(query, (data[0]))
-
-    # Esegui il commit delle modifiche
-    conn2.commit()
+        # Esegui il commit delle modifiche
+        conn2.commit()
+    except sqlite3.Error as e:
+        print("Errore durante l'esecuzione della query: {e}")
 
     # Chiudi la connessione
     #conn.close()
