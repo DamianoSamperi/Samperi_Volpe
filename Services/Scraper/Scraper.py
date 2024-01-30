89f21a4ae8044a69b835c13e5ee74dfb1d15ed07
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2024-01-30 16:04:05 +0100

    .

--- Services/Scraper/Scraper.py
+++ Services/Scraper/Scraper.py
@@ -100,58 +100,6 @@ def recupero_aeroporti():
     return aeroporti
 
 
-#TO_DO Possibilità utilizzo thread
-# lock = threading.Lock()
-# def richiesta_api(richiesta,data,data_domani):
-#     # Acquisisci il lock
-#     with lock:
-#         if richiesta=='tratta':
-#             return amadeus.shopping.flight_offers_search.get(originLocationCode=data[1], destinationLocationCode=data[2], departureDate= data_domani, adults=data[3])
-#         elif richiesta== 'aroporto':
-#             return amadeus.shopping.flight_destinations.get(origin=data[0],oneWay=True,nonStop=True)  
-#         time.sleep(0.1)
-
-# def Richiesta_API_Tratta():
-#     domani = datetime.now() + timedelta(days=1) 
-#     data_domani = domani.strftime('%Y-%m-%d')
-
-#     response=requests.post('http://controllertratta-service:5002/invio_Scraper', json={'request':'tratta'})
-#     if  response != 'error':
-#       tratte = response
-#     for tratta in tratte:
-#         try:
-#             response = richiesta_api('tratta',tratta,data_domani) 
-#             data = trova_prezzo_tratta(response,tratte[0],tratte[1])
-#             inviotratta(data) #funzione che permette di inviare al topic kafka la tratta ottenuta
-#         except ResponseError as error:
-#             raise error
-#         time.sleep(86400)
-
-# def Richiesta_API_Aeroporto():
-#     domani = datetime.now() + timedelta(days=1) 
-#     data_domani = domani.strftime('%Y-%m-%d')
-#     response = requests.post('http://controllertratta-service:5002/invio_Scraper', json={'request':'aeroporto'})
-#     if response != 'error':
-#         aeroporti = response
-#     for aeroporto in aeroporti:
-#         try:
-#             response = richiesta_api('aeroporto',aeroporto,data_domani)   
-#             data = trova_prezzo_aeroporto(response)
-#             invioaeroporto(response) #funzione che permette di inviare al topic kafka la tratta ottenuta
-#         except ResponseError as error:
-#             raise error
-#         time.sleep(86400)
-
-# # Creazione del primo thread
-# t1 = threading.Thread(target=Richiesta_API_Tratta)
-
-# # Creazione del secondo thread
-# t2 = threading.Thread(target=Richiesta_API_Aeroporto)
-
-# # Avvio dei thread
-# t1.start()
-# t2.start()
-
 @circuit(failure_threshold=5,recovery_timeout=43200)
 def chiedi_tratte_controller_tratte():
     try:
