07a4eaa0f1ea4b2a1b26dfbb2b56173f411fcf71
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2024-01-30 16:08:17 +0100

    puliti alcuni commenti in SLAmanager

--- Services/SLAmanager/SLAmanager.py
+++ Services/SLAmanager/SLAmanager.py
@@ -19,9 +19,7 @@ import cufflinks as cf
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
-#tempo di risposta di ogni api e consumo di risorse
-#response time e consumo di cpu
-#misura a rules
+
 app=Flask(__name__)
 
 while(True):
@@ -34,37 +32,6 @@ while(True):
         time.sleep(10)
 
 prometheus_url="http://prometheus-service:9090"
-#da sistemare le soglie
-# metrics = [
-#     {'nome': 'node_network_receive_errs_total', 'soglia': 2, 'desiderato': 0},
-#     {'nome': 'node_network_transmit_errs_total', 'soglia': 2, 'desiderato': 0},
-#     {'nome': 'node_memory_MemAvailable_bytes', 'soglia': 884879400, 'desiderato': 884879360},
-#     #{'nome': 'kafka_brokers', 'soglia': 1, 'desiderato': 1},
-#     {'nome': 'kafka_consumergroup_members', 'soglia': 2, 'desiderato': 2},
-#     {'nome': 'count(kube_persistentvolume_created)', 'soglia': 8, 'desiderato': 8},
-#     {'nome': 'count(kube_service_created)', 'soglia': 21, 'desiderato': 21},
-#     {'nome': 'prometheus_sd_kubernetes_http_request_duration_seconds_count', 'soglia': 5, 'desiderato': 1},
-#     #{'nome': 'prometheus_sd_http_failures_total', 'soglia': 0, 'desiderato': 0},
-#     {'nome': 'container_cpu_usage_seconds_total{pod=~"rules.*"}', 'soglia': 7, 'desiderato': 5},
-#     {'nome': 'scraping_time', 'soglia': 4, 'desiderato': 3},
-#     {'nome': 'elaborating_tratte_time', 'soglia': 11, 'desiderato': 10},
-#     {'nome': 'elaborating_aeroporti_time', 'soglia': 11, 'desiderato': 10}
-# ]
-
-# #TO_DO forse conviene che inseriamo le metriche base direttamente nello script sql
-# delete_query = f'DELETE FROM metriche'
-# cursor.execute(delete_query)
-# conn.commit()
-
-# for metric in metrics:
-#     try:
-#         cursor.execute('''
-#         INSERT INTO metriche (nome, soglia, desiderato)
-#         VALUES (%s, %s, %s)
-#         ''', (metric['nome'], metric['soglia'], metric['desiderato']))
-#         conn.commit()
-#     except mysql.connector.Error as e:
-#         print(f"Errore durante l'esecuzione della query: {e}")
 metriche_personalizzate = ['scraping_time','elaborating_tratte_time','elaborating_aeroporti_time']
 
 def contains(data,lista):
@@ -84,21 +51,17 @@ def get_metrics_list():
     except mysql.connector.Error as e:
         print(f"Errore durante l'esecuzione della query: {e}")
     valori = [tupla[0] for tupla in metriche]
-    # metriche = ', '.join(valori) TO_DO che senso ha?
     return valori
 
 #prende il valore attuale delle metriche
 @app.route('/get_valori_attuali', methods=['POST'])
 def fetch_prometheus_metrics():
     try:
-        #query a prometheus con la lista di metriche
         queries=get_metrics_list()
         prom = PrometheusConnect(url=prometheus_url, disable_ssl=True)
         # Esegui la query per ottenere le metriche specifiche
         result={}
         for query in queries:
-          #result.append(prom.custom_query(query))
-          #dovrebbe funzionare così
           result[query]=prom.custom_query(query)
           if result[query]==[]:
             result[query]="campioni insufficienti"
@@ -130,11 +93,7 @@ def aggiungi_metrica():
     if request.method == 'POST': 
         data = request.json
         try:
-            #query a prometheus con la lista di metriche
             prom = PrometheusConnect(url=prometheus_url, disable_ssl=True)
-            # Esegui la query per ottenere le metriche specifiche
-            #result.append(prom.custom_query(query))
-            #dovrebbe funzionare così
             result=prom.custom_query(data['nome'])
             if result!=[]:
                 try:
@@ -162,18 +121,17 @@ def get_valori_desiderati():
     except mysql.connector.Error as e:
         print(f"Errore durante l'esecuzione della query: {e}")
         return e
-    return metriche #in teoria va bene così, non penso che devo creare un dizionario
+    return metriche
 
 #ritorna se c'è stata violazione o no per ogni metrica
 @app.route('/get_stato_attuale', methods=['POST'])
-def get_violazioni(): #TO_DO da sistemare in base ai label che mi torna prometheus
+def get_violazioni():
     violazioni={}
     valori=fetch_prometheus_metrics()
     #scorro sia la chiave che il valore del mio dizionario
     for metrica,valore in valori.items():
         try:
             print("valore attuale ",valore,"\n")
-            #nome_metrica=valore[0]["metric"]["__name__"]
             nome_metrica=metrica
             query="SELECT soglia FROM metriche WHERE nome=%s"
             cursor.execute(query, (nome_metrica,))
@@ -189,7 +147,7 @@ def get_violazioni(): #TO_DO da sistemare in base ai label che mi torna promethe
         except Exception as e:
             print(f"Errore durante l'esecuzione : {e}")
             return e
-    return violazioni #forse meglio tornare un json?
+    return violazioni
 
 #ritorna il numero di violazioni in un arco di tempo
 @app.route('/get_violazioni_tempo', methods=['POST'])
@@ -200,7 +158,7 @@ def get_violazioni_tempo():
         # Connessione a Prometheus
         prom = PrometheusConnect(url=prometheus_url)
         end=datetime.utcnow()
-        start=end-timedelta(hours=data['ore']) #vedi se è giusto data["ore"]
+        start=end-timedelta(hours=data['ore'])
         try:
             query="SELECT nome, soglia FROM metriche"
             cursor.execute(query)
@@ -209,10 +167,6 @@ def get_violazioni_tempo():
             print(f"Errore durante l'esecuzione della query: {e}")
         for metrica in metriche:
             try:
-                # Costruzione della query per ottenere il conteggio delle violazioni
-                # query = f'count_over_time({metrica[0]} > {metrica[1]})'
-                # query = f'count(rate(metrica[0][1h])>metrica[1])'
-                # query = f'count_over_time((rate(metrica[0][1h]) > metrica[1])[1h:])'
                 query = f'count_over_time((rate({metrica[0]}[1h:]) > {metrica[1]})[1h:])'
                 result = prom.custom_query_range(query, start_time=start, end_time=end, step='1h')
                 # Estrazione del valore dalla risposta
@@ -423,125 +377,5 @@ def get_probabilità_violazioni():
         return f"probabilità violazione nei prossimi {data['minuti']} minuti = {probability_of_violation}%"
         # return "grafico creato"
 
-                
-        '''
-        plt.plot(df.index, df['value'], label='Original Data')
-        plt.plot(pd.date_range(df.index[-1], periods=forecast_steps+1, freq='60s')[1:], forecast, label='Forecast', color='red')
-        plt.legend()
-        plt.show()
-        '''
-        #Elio
-        '''
-        dictPred={}
-        metrics=get_metrics_list()
-        for name in metrics:
-            #rappresenta una query response come un data frame
-            #metric_df = MetricRangeDataFrame(Metrics[name])
-
-            data= metric_data.resample(rule='5s').mean(numeric_only="True")
-
-            tsmodel = ExponentialSmoothing(data['value'].interpolate(), trend='add', seasonal='add',seasonal_periods=5).fit()
-            
-            prediction = tsmodel.forecast(steps=round((10*60)/5))
-
-            # plt.figure(figsize=(24,10))
-            # plt.ylabel('Values', fontsize=14)
-            # plt.xlabel('Time', fontsize=14)
-            # plt.title('Values over time', fontsize=16)
-            
-            # plt.plot(data, "-", label = 'train')
-            # plt.plot(prediction,"--",label = 'pred')
-            # plt.legend(title='Series')
-            # # plt.show()
-            # plt.savefig(fname=name)
-
-            dictPred[name]={"max": prediction.max(),
-                        "min": prediction.min(),
-                        "avg": prediction.mean()}
-        return dictPred
-        '''
-
-        #modo con ARIMA
-        '''
-        # Creo un DataFrame pandas con i dati della metrica
-        df = pd.DataFrame(columns=['timestamp'] + metrics)
-        for entry in metric_data:
-            timestamp = pd.to_datetime(entry['value'][0])
-            values = [float(entry['value'][1])]  # Assumendo che il valore della metrica sia un numero
-            df = pd.concat([df, pd.DataFrame([timestamp] + values, columns=['timestamp'] + metrics)])
-
-        # Addestramento del modello ARIMA
-        for metric in metrics:
-            train_data = df[metric]
-            model = ARIMA(train_data, order=(1, 1, 1))  # Sostituisci con i tuoi parametri
-            fitted_model = model.fit()
-
-            # Previsioni per i prossimi 5 minuti (modifica in base alle tue esigenze)
-            forecast_steps = data['minuti']
-            future_timestamps = pd.date_range(end_time, periods=forecast_steps+1, freq='60s')[1:]
-            predictions = fitted_model.forecast(steps=forecast_steps)
-
-            try:
-                query="SELECT soglia FROM metriche WHERE nome= ?"
-                cursor.execute(query, (metric,))
-                threshold = cursor.fetchone()
-            except mysql.connector.errors as e:
-                print(f"Errore durante l'esecuzione della query: {e}")
-                return e            
-            # Calcolo della probabilità di violazione della soglia
-            probability = calculate_probability(predictions, threshold)
-
-            probabilities[metric]=probability
-        return probabilities
-        '''
-    
-        #modo con media e deviazione standard
-        '''
-        #calcolo le probabilità di violazioni in base a ciò che ho ottenuto
-        probabilities = {}
-        for entry in metric_data:
-            values = [value[1] for value in entry['values']]
-            mean = np.mean(values) #media
-            std_dev = np.std(values) #deviazione standard
-            try:
-                query="SELECT soglia FROM metriche WHERE nome= ?"
-                cursor.execute(query, (entry['name'],)) #TO_DO vedi se è name
-                threshold = cursor.fetchone()
-            except mysql.connector.Error as e:
-                print(f"Errore durante l'esecuzione della query: {e}")
-                return e
-            z_score = (threshold - mean) / std_dev #z-score per la soglia
-            #probabilità di violazione usando la distribuzione normale
-            probability = norm.cdf(z_score)
-            probability_next_interval = 1 - (1 - probability) ** data['minuti']
-            probabilities[entry['name']]=probability_next_interval
-        return probabilities
-        '''
-
-#con menu, ma dovrebbe essere più giusto con la route, come sopra
-'''
-#aggiunge o rimuove metriche ad SLA TO_DO probabilmente così non va bene e devo farlo con la route
-def aggiorna_metriche():
-    print("Ecco le metriche attuali del SLA \n")
-    metric_list=get_metrics_list()
-    print(metric_list)
-    scelta=int(input("Inserisci 1 se vuoi eliminare una metrica, 2 se vuoi aggiungerla, qualsiasi altro numero se vuoi uscire "))
-    if scelta==1:
-        nome=input("Scrivi il nome della metrica da eliminare ")
-        query="DELETE FROM metriche WHERE nome=?"
-        cursor.execute(query,(nome,))
-        conn.commit()
-    elif scelta==2:
-        nome=input("Scrivi la metrica da aggiungere ")
-        soglia=input("Scrivi la soglia della metrica da aggiungere ")
-        desiderato=input("Scrivi il valore desiderato della metrica da aggiungere ")
-        cursor.execute('''
-        #INSERT INTO metriche (nome, soglia, desiderato)
-        #VALUES (?, ?, ?)
-        #''', (nome, soglia, desiderato))
-        #conn.commit()
-    #else:
-    #    return "Non hai eliminato nè aggiunto niente"
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5014, debug=True, threaded=True) #vedi numero porta
+    app.run(host="0.0.0.0",port=5014, debug=True, threaded=True)
